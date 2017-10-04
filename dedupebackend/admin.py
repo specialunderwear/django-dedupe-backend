a6b464e8d48e9528f313d269c524426c6ca32274
Lars van de Kerkhof <lars@permanentmarkers.nl>
2017-10-04 13:53:34 +0200

    you can't add these things through the admin.

--- dedupebackend/admin.py
+++ dedupebackend/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from dedupebackend.fields import UniqueFileAdminField, UniqueFileAdminWidget
 from dedupebackend.models import UniqueFile
 from dedupebackend.utils import sizeof_fmt
 from dedupebackend.filters import FileSizeFilter
@@ -8,6 +9,9 @@ class UniqueFileAdmin(admin.ModelAdmin):
     search_fields = ('original_filename', 'filename')
     list_display = ('original_filename', 'mime_type', 'filesize', 'id', 'tiny_thumb')
     list_filter = ('mime_type', FileSizeFilter)
+
+    def has_add_permission(self, request):
+        return False
 
     def filesize(self, obj):
         if obj.size is not None:
