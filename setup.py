9136d0ba0d43e412d01bab37b881ae82352b7652
Lars van de Kerkhof <lars@permanentmarkers.nl>
2017-11-30 10:06:06 +0100

    Added licence.

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 """
-Django git backend - the git storage backend
+Django dedupe backend - store files only once
 
-This thing stores your files in git. That way your files are automatically
-deduplicated.
+This thing stores your files in hash folder structure like git.
+Files with the same hash will never be duplicated
 """
 from setuptools import setup, find_packages
 
 
 __version__ = "0.0.1"
 
+def readme():
+    try:
+        return open('README.rst').read()
+    except:
+        return __doc__
 
 setup(
     # package name in pypi
@@ -16,13 +21,13 @@ setup(
     # extract version from module.
     version=__version__,
     description="The super storage backend",
-    long_description=__doc__,
+    long_description=readme(),
     classifiers=[],
     keywords='django storage backend that stores files in git',
     author='Lars van de Kerkhof',
     author_email='lars@permanentmarkers.nl',
-    url='',
-    license='',
+    url='https://github.com/specialunderwear/django-dedupe-backend',
+    license='GPL v3',
     # include all packages in the egg, except the test package.
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     # for avoiding conflict have one namespace for all apc related eggs.
